a7f915615943d4acd0c810feeedf0973edce9767
Lucas Lion <lucaslion7@gmail.com>
2023-09-01 17:55:13 +0200

    floating borders solid

--- after/plugin/cheat-sheet.lua
+++ after/plugin/cheat-sheet.lua
@@ -1,2 +1,19 @@
 
+require("cheat-sheet").setup({
+  auto_fill = {
+    filetype = true,
+    current_word = true,
+  },
+
+  main_win = {
+    style = "minimal",
+    border = "solid",
+  },
+
+  input_win = {
+    style = "minimal",
+    border = "solid",
+  },
+})
+
 vim.keymap.set("n", "<C-s>", ":CheatSH<CR>")
