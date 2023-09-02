815992b1547f7da6a3da34376a270f94b25e0093
Lucas Lion <lucaslion7@gmail.com>
2023-09-02 04:41:08 +0200

    DAPgit commit -m DAP

--- after/plugin/nvim-dap-ui.lua
+++ after/plugin/nvim-dap-ui.lua
@@ -1,16 +1,48 @@
 
-vim.keymap.set('n', "<leader>zc", "<cmd>lua require'dap'.continue()<CR>")
-vim.keymap.set('n', "<leader>zs", "<cmd>lua require'dap'.step_over()<CR>")
-vim.keymap.set('n', "<leader>zi", "<cmd>lua require'dap'.step_into()<CR>")
-vim.keymap.set('n', "<leader>zo", "<cmd>lua require'dap'.step_out()<CR>")
+vim.keymap.set('n', "<F2>", "<cmd>lua require'dap'.continue()<CR>")
+vim.keymap.set('n', "<F3>", "<cmd>lua require'dap'.step_into()<CR>")
+vim.keymap.set('n', "<F4>", "<cmd>lua require'dap'.step_over()<CR>")
+vim.keymap.set('n', "<F5>", "<cmd>lua require'dap'.step_out()<CR>")
 vim.keymap.set('n', "<leader>zb", "<cmd>lua require'dap'.toggle_breakpoint()<CR>")
-vim.keymap.set('n', "<leader>zB", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Condition du point d'arrêt: '))<CR>")
-vim.keymap.set('n', "<leader>zp", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Message du point de journalisation: '))<CR>")
-vim.keymap.set('n', "<leader>zr", "<cmd>lua require'dap'.repl.open()<CR>")
+vim.keymap.set('n', "<leader>zc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+vim.keymap.set('n', "<leader>zl", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
+vim.keymap.set('n', "<F8>", "<cmd>lua require'dap'.repl.open()<CR>")
+--vim.keymap.set('n', "<leader>zz", "<cmd>lua require'dapui'.open()<CR>")
 
-local dap = require('dap')
-dap.adapters.python = {
-	type = 'executable';
-	command = os.getenv('HOME') .. '/.virtualenvs/tools/bin/python';
-	args = { '-m', 'debugpy.adapter' };
+require("nvim-dap-virtual-text").setup()
+require("dapui").setup()
+require("dap").adapters.lldb = {
+	type = "executable",
+	command = "/usr/bin/lldb-vscode", -- adjust as needed
+	name = "lldb",
 }
+require("dap").configurations.cpp = {
+	{
+		name = "Launch",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input(
+				"Path to executable: ",
+				vim.fn.getcwd() .. "/",
+				"file"
+			)
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+		runInTerminal = false,
+	},
+}
+
+-- open and close dapui
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
