c36363bfdd26a1f8a50967731cc40d5b9ac55e98
Lucas Lion <lucaslion7@gmail.com>
2023-10-12 10:24:49 +0200

    shortcuts and telescope normal mode

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>n', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fc', builtin.commands, {})
@@ -19,7 +20,7 @@ end)
 
 require('telescope').setup{
   defaults = {
-    -- ...
+	  initial_mode = "normal",
   },
   pickers = {
     find_files = {
