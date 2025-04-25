83a89a62f2c8998c33a0878c21d50d2f59e76648
Lucas Lion <lucaslion7@gmail.com>
2025-04-25 13:54:15 +0200

    nvimtree  rules

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -16,9 +16,9 @@ require("nvim-tree").setup({
     group_empty = true,
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
   },
   git = {
-    ignore = true,
+    ignore = false,
   },
 })
