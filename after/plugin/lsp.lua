1c9cb10dceb3a169aa725256a224b6176ed9a40b
Lucas Lion <lucaslion7@gmail.com>
2026-01-14 13:42:01 +0100

    lsp update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,61 +1,7 @@
-local lsp = require("lsp-zero")
+local lsp_zero = require("lsp-zero")
 
-lsp.preset("recommended")
-
--- Mason et mason-lspconfig pour gérer l'installation des LSP
-local mason = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
-
-mason.setup()
-mason_lspconfig.setup({
-    ensure_installed = { "clangd" },
-    automatic_installation = true,
-})
-
--- Configurer lua_ls pour éviter l'erreur 'Undefined global vim'
-local lspconfig = require("lspconfig")
-lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
-})
-
--- Config nvim-cmp (auto-complétion)
-local cmp = require("cmp")
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = {
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ['<C-Space>'] = cmp.mapping.complete(),
-}
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-cmp.setup({
-  mapping = cmp_mappings,
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-  },
-})
-
--- Preferences lsp-zero
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = "E",
-        warn = "W",
-        hint = "H",
-        info = "I",
-    },
-})
-
-lsp.on_attach(function(client, bufnr)
+-- lsp-zero v3.x on_attach
+lsp_zero.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
@@ -68,7 +14,49 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
-lsp.setup()
+-- Mason setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "clangd", "lua_ls" },
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      require("lspconfig").lua_ls.setup({
+        settings = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+          },
+        },
+      })
+    end,
+  },
+})
+
+-- Config nvim-cmp (auto-complétion)
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+  },
+})
+
+-- Sign icons
+lsp_zero.set_sign_icons({
+  error = "E",
+  warn = "W",
+  hint = "H",
+  info = "I",
+})
 
 vim.diagnostic.config({
     virtual_text = true,
